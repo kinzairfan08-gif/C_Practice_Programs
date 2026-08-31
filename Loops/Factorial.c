44520e3ecc7f5a3e1f59ac6810b2f95420178591
kinzairfan08-gif <kinzairfan08@gmail.com>
2026-08-31 18:02:48 +0500

    Add factorial calculation in Factorial.c
    Implement a program to calculate the factorial of a number using a loop.

--- Loops/Factorial.c
+++ Loops/Factorial.c
@@ -1,1 +1,18 @@
+#include<stdio.h>
+
+int main(void)
+{
+	int f,n, a;
+	
+	printf("Enter a number: ");
+	scanf("%d", & n);
+	
+	a=1;
+	
+	for (f=n;f>=1; f--)
+	{
+		a=f*a;
+	}
+	printf("The factorial of the given number is: %d",a);
+}
 
