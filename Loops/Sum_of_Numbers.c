48c9b37e9a710d79bed1b0287a5d8b4192c785c6
kinzairfan08-gif <kinzairfan08@gmail.com>
2026-08-31 18:01:17 +0500

    Add sum of numbers program in Sum_of_Numbers.c
    Implement a program to calculate the sum of numbers from 1 to n.

--- Loops/Sum_of_Numbers.c
+++ Loops/Sum_of_Numbers.c
@@ -1,1 +1,18 @@
+#include<stdio.h>
+
+int main(void)
+
+{
+	int n,a, sum;
+	
+	printf("Enter a number: ");
+	scanf("%d",&n);
+	
+	sum = 0;
+	for (a=1;a<=n;a++)
+	{
+		sum+=a;
+	}
+	printf("The sum of the give number to 1 is: %d",sum);
+}
 
