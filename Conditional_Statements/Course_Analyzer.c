d1d9b75390210020a91c0717091ef7cb25fa420e
kinzairfan08-gif <kinzairfan08@gmail.com>
2026-09-10 00:43:27 +0500

    Modifying Course Analyzer
    Refactored course analyzer to simplify pass/fail counting and output.

--- Conditional_Statements/Course_Analyzer.c
+++ Conditional_Statements/Course_Analyzer.c
@@ -3,7 +3,12 @@
 int main (void)
 {
 	int math, english, urdu, computer, pst, total_score, pass, fail, average;
-	printf("Enter your Maths Marks: ");
+	
+	
+	printf("\tCourse Analyzer!\n");
+	printf("*********************************\n");
+	
+	printf("\n\nEnter your Maths Marks: ");
 	scanf("%d",& math);
 	printf("Enter your English Marks: ");
 	scanf("%d",& english);
@@ -14,104 +19,70 @@ int main (void)
 	printf("Enter your Pak.Studies Marks: ");
 	scanf("%d",& pst);
 	
+	
+	pass = 0;
+	fail = 0;
+	
 	if (math >= 50)
 	{
-	printf ("Math: PASS\n");
+	printf ("\nMath: PASS\n");
+	pass+=1;
 	}
 	else
 	{
-	printf ("Math: FAIL\n");
+	printf ("\nMath: FAIL\n");
+	fail+=1;
 	}
 	
 	if (english >= 50)
 	{
 	printf ("English: PASS\n");
+	pass+=1;
 	}
 	else
 	{
 	printf ("Englis: FAIL\n");
+	fail+=1;
 	}
 	
 	if (urdu >= 50)
 	{
 	printf ("Urdu: PASS\n");
+	pass+=1;
 	}
 	else
 	{
 	printf ("Urdu: FAIL\n");
+	fail+=1;
 	}
 	
 	if (computer >= 50)
 	{
 	printf ("Computer: PASS\n");
+	pass+=1;
 	}
 	else
 	{
 	printf ("Computer: FAIL\n");
+	fail+=1;
 	}
 	
 	if (pst >= 50)
 	{
 	printf ("Pak.Studies: PASS\n");
+	pass+=1;
 	}
 	else
 	{
 	printf ("Pak.Studies: FAIL\n");
+	fail+=1;
 	}
 
 	average = math+english+computer+urdu+pst;
 	printf("Your average score is: %d\n", average);
 	
-	pass = 0;
+	printf ("\nThe total number of subjects you've passed are %d\n", pass);
 	
-	if (math>=50)
-	{
-		pass+=1;
-	}
-	if (english>=50)
-	{
-		pass+=1;
-	}
-	if (urdu>=50)
-	{
-		pass+=1;
-	}
-	if (computer>=50)
-	{
-		pass+=1;
-	}
-	if (pst>= 50)
-	{
-		pass+=1;
-	}
-	
-	printf ("The total number of subjects you've passed are %d\n", pass);
-	
-	fail = 0;
-	
-	if (math<=50)
-	{
-		fail+=1;
-	}
-	if (english<=50)
-	{
-		fail+=1;
-	}
-	if (urdu<=50)
-	{
-		fail+=1; 
-	}
-	if (computer<=50)
-	{
-		fail+=1;
-	}
-	if (pst<=50)
-	{
-		fail+=1;
-	}
-	
-	printf ("The total number of subjects you've failed are %d\n", fail);
-	
+	printf ("\nThe total number of subjects you've failed are %d\n", fail);
 	
 }
-
